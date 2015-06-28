72f97efe7707d6d968002aa0af84f0b9993abc15
Ankit Arun <talk.ankit373@gmail.com>
2015-06-27 20:33:37 -0700

    init

--- infinTee/extractTweets.py
+++ infinTee/extractTweets.py
@@ -1,20 +1,40 @@
 __author__ = 'ankit'
 
 import twitterConfig
+import yweather
 
 twitter_api = twitterConfig.TwitterLogin().login_to_twitter()
-# US_WOE_ID = 23424977
-# us_trends = twitter_api.trends.place(_id=US_WOE_ID)
 
 
-def get_tweets(location):
+def get_woeid(location):
 
-    response = twitter_api.GetSearch(term=location, count=100)
-    print response
+    client = yweather.Client()
+    return client.fetch_woeid(location)
+
+
+def get_tweets(woeid_str):
+
+    woeid = int(woeid_str.strip())
+    trends = twitter_api.trends.place(_id=woeid)
+    trends_set = set([trend['name']
+                    for trend in trends[0]['trends']])
+    tweets_for_each_trending_topic = dict()
+    for trend in trends_set:
+        tweets = twitter_api.search.tweets(q=trend, count=1)
+        temp = []
+        for tweet in tweets:
+            temp.append(tweet)
+        tweets_for_each_trending_topic[trend] = tweets['statuses'][0]['text'].encode("utf8")
+    print tweets_for_each_trending_topic
+
+
 
 
 if __name__ == '__main__':
-    get_tweets('Toronto')
+    woeid = get_woeid('India')
+    # print woeid
+    get_tweets(woeid)
+    # print type(US_WOE_ID)
 
 
 
