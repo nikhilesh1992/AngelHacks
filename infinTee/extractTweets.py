4ed40b65ac2ceceb532dd6350175703f68455c7c
Ankit Arun <talk.ankit373@gmail.com>
2015-06-27 18:32:50 -0700

    init

--- infinTee/extractTweets.py
+++ infinTee/extractTweets.py
@@ -1,1 +1,20 @@
 __author__ = 'ankit'
+
+import twitterConfig
+
+twitter_api = twitterConfig.TwitterLogin().login_to_twitter()
+# US_WOE_ID = 23424977
+# us_trends = twitter_api.trends.place(_id=US_WOE_ID)
+
+
+def get_tweets(location):
+
+    response = twitter_api.GetSearch(term=location, count=100)
+    print response
+
+
+if __name__ == '__main__':
+    get_tweets('Toronto')
+
+
+
